b0c85c6959f95ca05fe8e8b84b3d134ceb29449a
hungbn041204 <118287834+hungbn041204@users.noreply.github.com>
2022-12-26 21:40:25 +0700

    Update qlymobile.py

--- NHOM11/qlymobile.py
+++ NHOM11/qlymobile.py
@@ -1,7 +1,6 @@
-#mở và tạo file
+#mở file
 import csv
 _path="D:/NHOM11/files/ds_mobile.csv"
 with open(_path,'w',encoding = 'utf-8') as f:
-        f.write('Mã điện thoại','Tên điện thoại','đơn giá','Số Lượng','Chất lượng','Thuế VAT','Tổng tiền')
+        f.write('Mã điện thoại, Tên điện thoại, đơn giá, số Lượng, Chất lượng, Thuế VAT, Tổng tiền')
         f.close()
-
