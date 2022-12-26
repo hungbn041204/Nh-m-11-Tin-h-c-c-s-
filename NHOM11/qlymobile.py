221d0ca545fdba1f9aa3109c3b81301f61c19deb
hungbn041204 <118287834+hungbn041204@users.noreply.github.com>
2022-12-26 21:20:37 +0700

    Update qlymobile.py

--- NHOM11/qlymobile.py
+++ NHOM11/qlymobile.py
@@ -4,16 +4,10 @@ import csv
 _path="files/ds_mobile.csv"
 List_mobile=[]
 
-#lưu và tạo
-def luu_ds_hoa_don(_path,List_mobile):
-    try:
-        f=open(_path,'w+',newline='', encoding = 'utf-8')
-        csv.writer(f).writerow(['Mã điện thoại','Tên điện thoại','đơn giá','Số Lượng','Chất lượng','Thuế VAT','Tổng tiền','tam_ung','con_lai'])
-        for hd in List_mobile:
-            csv.writer(f).writerow([hd['Mã điện thoại'],hd['Tên điện thoại'], hd['đơn giá'],hd['Số lượng'],hd['Chất lượng'],hd['Thuế VAT'],hd['Tổng tiền']])
+#mở và tạo file
+import csv
+_path="D:/NHOM11/files/ds_mobile.csv"
+with open(_path,'w',encoding = 'utf-8') as f:
+        f.write('Mã điện thoại','Tên điện thoại','đơn giá','Số Lượng','Chất lượng','Thuế VAT','Tổng tiền')
         f.close()
-        return 1
-    except Exception as ex1:
-        return 0
-
 
