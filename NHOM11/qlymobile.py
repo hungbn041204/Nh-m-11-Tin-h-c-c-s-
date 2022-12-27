69a28de7692dc2cc453c766013239626e8167567
hungbn041204 <118287834+hungbn041204@users.noreply.github.com>
2022-12-27 08:24:26 +0700

    Update qlymobile.py

--- NHOM11/qlymobile.py
+++ NHOM11/qlymobile.py
@@ -1,7 +1,45 @@
-import csv
-_path="D:/NHOM11/files/ds_mobile.csv"
-listmobile = []
+#Quản lý hóa đơn
+import os, csv
+_path="files/ds_mobile.csv"
+lstmobile=[]
 
+def mo_file_hoa_don(_path,lstmobile):
+    try:
+        f=open(_path,'w', encoding ='utf-8')
+        for dong in csv.reader(f):
+            if dong[0]=='ma_dt':
+                continue
+            lstmobile.append({'ma_dt':dong[0],'ten_dt':dong[1], 'don_gia':dong[2],'so_luong':dong[3],'chat_luong':dong[4],'thue_vat':dong[5],'tong_tien':dong[6]})
+        f.close()
+        return 1
+    except Exception as ex1:
+        print('Khong mở được file hop le ', ex1)
+    return
+
+def luu_ds_mobile(_path,lstHoaDon):
+    try:
+        f=open(_path,'w',newline='', encoding = 'utf-8')
+        csv.writer(f).writerow(['ma_dt','ten_dt','don_gia','so_luong','Chat_luong','thue_vat','tong_tien'])
+        for hd in lstmobile:
+            csv.writer(f).writerow([hd['ma_dt'],hd['ten_dt'], hd['don_gia'],hd['so_luong'],hd['chat_luong'],hd['thue_vat'],hd['tong_tien']])
+        f.close()
+        return 1
+    except Exception as ex1:
+        return 0
+
+def them_hoa_don(lstmobile):
+    while True:
+        ma_dt = input('Nhập số mã điện thoại: ')
+        ten_dt=input('Nhập tên điện thoại: ')
+        don_gia=input('Nhập đơn giá: ')
+        so_luong=input('Nhập số lượng: ')
+        chat_luong=input('Nhập chất lượng: ')
+        lstmobile.append({'ma_dt':ma_dt,'ten_dt':ten_dt,\
+             'don_gia':don_gia,'so_luong':so_luong,'chat_luong':chat_luong}) 
+        tt=input('Bạn có muốn tiếp tục thêm ? (1:TT)')
+        if tt!='1':
+            break
+    return
 
 print("+--------------------------------------------------+")
 print("|                    Menu chức năng                |")
@@ -12,32 +50,11 @@ print("|                 4: Lưu danh sách                 |")
 print("|                 5: Sắp xếp cửa hàng              |")
 print("+--------------------------------------------------+")
 
-chuc_nang = int(input("Nhập chức năng bạn muốn chọn(1->5)"))
+chucnang = int(input("Nhập chức năng bạn muốn chọn(1->5)"))
 
-
-#mở file
-with open(_path,'w',encoding = 'utf-8') as f:
-        f.write('Mã điện thoại, Tên điện thoại, đơn giá, số Lượng, Chất lượng, Thuế VAT, Tổng tiền')
-        f.close()
-
-#nhập thông tin
-tt_dien_thoai = []
-while True:
-        ma_dt = input('Nhập mã điện thoại:')
-        ten_dt = input('Nhập tên điện thoại:')
-        don_gia = input('Nhập đơn giá:')
-        so_luong = input('Nhập số lượng:')
-        chat_luong = input('Chất lượng:')
-        tt_dien_thoai.append({'Mã điện thoại':ma_dt,'Tên điện thoại':ten_dt,\
-             'Đơn giá':don_gia,'Số lượng':so_luong,'Chất lượng':chat_luong})
-        print(tt_dien_thoai)
-        break
-
-def tinh_vat(x):
-        if chat_luong==1:
-                x = (don_gia*so_luong/100*30)
-        elif chat_luong==2:
-                x = (don_gia*so_luong/100*10)
-                
-def tong_tien(tong):
-        tong = don_gia*so_luong             
+if chucnang==1:
+    mo_file_hoa_don(_path,lstmobile)
+elif chucnang==2:
+    luu_ds_mobile(_path,lstmobile)
+elif chucnang==3:
+    them_hoa_don(lstmobile)      
